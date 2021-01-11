f497657ca05da74b381c68feb162e156da812416
sahilthakur7 <sahilrd07@gmail.com>
2021-01-11 18:09:48 +0530

    Written queries for star wars APIs

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import Navbar from "./Components/Navbar";
 import People from "./Components/People";
 import Planets from "./Components/Planets";
 
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { useState } from "react";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState("People");
   return (
-    <div className="App">
-      <h1>Star Wars</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "People" ? <People /> : <Planets />}
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "People" ? <People /> : <Planets />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
